3ff2cc22a33a74a5340f7eacf557eb8ccaa4494b
Paulyn Ompico <ompicopaulyn@gmail.com>
2024-03-12 18:34:21 +0100

    Improve summary function with key facts

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,21 +9,50 @@ class Article < ApplicationRecord
     io = ActiveStorage::Blob.service.path_for(document.key)
     reader = PDF::Reader.new(io)
     return reader.page_count
-
-  #   # puts reader.pdf_version
-  #   # puts reader.info
-  #   # puts reader.metadata
-  #   # puts reader.page_count
   end
 
   def info
     io = ActiveStorage::Blob.service.path_for(document.key)
     reader = PDF::Reader.new(io)
-    i = reader.page(4).text.to_s
+    i = ""
+    if reader.page_count >= 10 
+      i += reader.page(4).text.to_s
+      i += reader.page(5).text.to_s
+      i += reader.page(7).text.to_s
+      i += reader.page(8).text.to_s
+      i += reader.page(9).text.to_s
+    else
+      reader.pages.each do |page|
+        i += page.text.to_s
+      end
+    end
     client = OpenAI::Client.new
     chaptgpt_response = client.chat(parameters: {
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: "Give the top 5 key facts from: #{i}"}]
+      messages: [{ role: "user", content: "Give the top 5 key facts from: #{i}. 1 sentence per fact."}]
+    })
+    return chaptgpt_response["choices"][0]["message"]["content"]
+  end
+
+  def summary
+    io = ActiveStorage::Blob.service.path_for(document.key)
+    reader = PDF::Reader.new(io)
+    i = ""
+    if reader.page_count >= 10 
+      i += reader.page(4).text.to_s
+      i += reader.page(5).text.to_s
+      i += reader.page(7).text.to_s
+      i += reader.page(8).text.to_s
+      i += reader.page(9).text.to_s
+    else
+      reader.pages.each do |page|
+        i += page.text.to_s
+      end
+    end
+    client = OpenAI::Client.new
+    chaptgpt_response = client.chat(parameters: {
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: "Summarize #{i} as concisely as possible."}]
     })
     return chaptgpt_response["choices"][0]["message"]["content"]
   end
