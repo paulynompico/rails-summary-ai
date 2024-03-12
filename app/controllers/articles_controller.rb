2a5bd2738f31705e05c241996270ac7314181825
Paulyn Ompico <ompicopaulyn@gmail.com>
2024-03-12 15:04:37 +0100

    Add PDF upload and display

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -65,6 +65,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:title, :url)
+      params.require(:article).permit(:title, :url, :document)
     end
 end
