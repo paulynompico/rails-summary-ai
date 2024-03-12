a60de17c3b415050de8328acc72fdf40f51c41c0
Paulyn Ompico <ompicopaulyn@gmail.com>
2024-03-12 14:59:42 +0100

    Add Active Storage with file upload

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,13 @@
 class Article < ApplicationRecord
+  has_one_attached :document
+
+  def content
+    client = OpenAI::Client.new
+    chaptgpt_response = client.chat(parameters: {
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: "What is #{title}? Return one sentence without opinions or comments. Just facts."}]
+    })
+    return chaptgpt_response["choices"][0]["message"]["content"]
+  end
+
 end
